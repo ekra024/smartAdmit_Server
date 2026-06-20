47ee3a8b4030cfa7081cebe68e018bfe7c6c644d
ekra024 <ekra.cse8.bu@gmail.com>
2026-06-20 09:35:12 +0600

    define roles constants

---
+++ src/constants/roles.js
@@ -0,0 +1,11 @@
+// src/constants/roles.js
+
+const ROLES = Object.freeze({
+  STUDENT: "STUDENT",
+  ADMIN: "ADMIN",
+  DEPARTMENT_OFFICER: "DEPARTMENT_OFFICER",
+  DEAN: "DEAN",
+  REGISTRAR: "REGISTRAR",
+});
+
+module.exports = ROLES;
